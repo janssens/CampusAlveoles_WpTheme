f3a253a43b78357981535ccb7965e947fd869eaf
Gaetan Janssens <janssensgaetan@gmail.com>
2021-03-25 16:14:15 +0100

    top menu

--- functions.php
+++ functions.php
@@ -4,13 +4,14 @@ function enqueue_styles() {
     wp_enqueue_style( 'parent-style', get_template_directory_uri().'/style.css' );
     wp_enqueue_style( 'child-style', get_stylesheet_uri() );
 }
-add_action( 'init', 'register_my_menu' );
-function register_my_menu() {
-    register_nav_menu('login-menu',__( 'Login Menu' ));
-}
+
 function print_menu_shortcode($attributes, $content = null) {
     extract(shortcode_atts(array( 'name' => null, 'class' => null ), $attributes));
-    return wp_nav_menu( array( 'menu' => $name, 'menu_class' => $class, 'echo' => false ) );
+    if (!$class){
+        $class = 'content_menu';
+    }
+    if (wp_get_nav_menu_object($name)){
+        return wp_nav_menu( array( 'menu' => $name, 'menu_class' => $class, 'echo' => false ) );
+    }
 }
-
 add_shortcode('menu', 'print_menu_shortcode');
